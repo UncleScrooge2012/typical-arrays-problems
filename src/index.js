5086beb5bb43e62f3cdee160c6b9681888eae420
UncleScrooge2012 <scrooge2024@yandex.ru>
2021-03-14 16:23:32 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+
+    if((array.length === 0) || (array ===  0)){
+        return 0
+    } 
+    let min = array[0]
+    
+    array.forEach(element => {
+        if (min > element) {
+            min = element
+        }
+        
+    })
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+    if((array.length === 0) || (array ===  0)){
+        return 0
+    } 
+    let max = array[0]
+    
+    array.forEach(element => {
+        if (max < element) {
+            max = element
+            
+        }
+        
+    })
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+    if((array.length === 0) || (array ===  0)){
+        return 0
+    } 
+    let sum = 0;    
+    let res;
+    for(let i = 0; i < array.length; i++){
+        sum += array[i]
+    }
+    res = sum / array.length  
+  return res;
 }
